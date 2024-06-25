f49d0ff1518aa6208990f9a3779554d8f8c06fcd
Enrique Ajin <enriquesaon@gmail.com>
2024-06-25 22:51:59 +0200

    fix(add-date-annotation): Add Require Api annotation in order to use date formatting functions.

--- app/src/main/java/com/enriqueajin/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/enriqueajin/newsapp/ui/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.enriqueajin.newsapp.ui
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -30,6 +32,7 @@ class MainActivity : ComponentActivity() {
     private val homeViewModel: HomeViewModel by viewModels()
     private val keywordNewsViewModel: KeywordNewsViewModel by viewModels()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
