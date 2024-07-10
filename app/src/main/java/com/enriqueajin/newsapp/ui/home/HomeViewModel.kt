423d1ad62ac004fdfaaa9d008abd160c7ee793c2
Enrique Ajin <enriquesaon@gmail.com>
2024-07-10 19:46:07 +0200

    Improvement(flows): Refactor the way flows are defined within HomeViewModel.

--- app/src/main/java/com/enriqueajin/newsapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/enriqueajin/newsapp/ui/home/HomeViewModel.kt
@@ -4,18 +4,23 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.enriqueajin.newsapp.data.network.model.NewsItem
 import com.enriqueajin.newsapp.domain.GetAllTopNewsUseCase
 import com.enriqueajin.newsapp.domain.GetNewsByKeywordUseCase
 import com.enriqueajin.newsapp.util.KeywordProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,8 +34,16 @@ class HomeViewModel @Inject constructor(
 
     var localState = MutableStateFlow(HomeLocalUiState(keyword = KeywordProvider.getRandomKeyword()))
 
+    private val _latestNews: Flow<List<NewsItem>> = getAllTopNewsUseCase(pageSize = "10")
+    private val _newsByKeyword: Flow<List<NewsItem>> = getNewsByKeywordUseCase(keyword = localState.value.keyword, pageSize = "15")
+    private val _newsByCategory: MutableStateFlow<List<NewsItem>> = MutableStateFlow(emptyList())
+
     private val _uiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
-    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+    val uiState: StateFlow<HomeUiState> = _uiState.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = HomeUiState.Loading
+    )
 
     init {
         getMainNews()
@@ -38,35 +51,15 @@ class HomeViewModel @Inject constructor(
 
     fun getMainNews() {
         combine(
-            getAllTopNewsUseCase(pageSize = "10"),
-            getNewsByKeywordUseCase(keyword = localState.value.keyword, pageSize = "15"),
-            localState
-        ) { latestNews, newsByKeyword, localState ->
-            when {
-                latestNews.isNullOrEmpty() -> {
-                    _uiState.value = HomeUiState.Success(
-                        newsByKeyword = newsByKeyword,
-                        categorySelected = localState.categorySelected,
-                        keywords = localState.keyword
-                    )
-                }
-                newsByKeyword.isNullOrEmpty() -> {
-                    _uiState.value = HomeUiState.Success(
-                        latestNews = latestNews,
-                        categorySelected = localState.categorySelected,
-                        keywords = localState.keyword
-                    )
-                }
-                else -> {
-                    _uiState.value = HomeUiState.Success(
-                        latestNews = latestNews,
-                        newsByKeyword = newsByKeyword,
-                        categorySelected = localState.categorySelected,
-                        keywords = localState.keyword
-                    )
-                }
-            }
-
+            _latestNews,
+            _newsByKeyword,
+            _newsByCategory
+        ) { latestNews, newsByKeyword, newsByCategory ->
+            _uiState.value = HomeUiState.Success(
+                latestNews = latestNews,
+                newsByKeyword = newsByKeyword,
+                newsByCategory = newsByCategory
+            )
         }.onStart {
             _uiState.value = HomeUiState.Loading
         }.catch {
@@ -76,18 +69,10 @@ class HomeViewModel @Inject constructor(
     }
 
     fun getNewsByCategory(category: String, pageSize: String) {
-        val currentState = (_uiState.value as? HomeUiState.Success)
-
         getAllTopNewsUseCase(category, pageSize).onStart {
             _uiState.value = HomeUiState.Loading
         }.onEach { news ->
-            _uiState.value = HomeUiState.Success(
-                latestNews = currentState?.latestNews,
-                newsByKeyword = currentState?.newsByKeyword,
-                newsByCategory = news,
-                categorySelected = category,
-                keywords = currentState?.keywords
-            )
+            _newsByCategory.value = news
         }.catch { error ->
             _uiState.value = HomeUiState.Error(error)
         }.launchIn(viewModelScope)
