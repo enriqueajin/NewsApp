820e2517fbbee9f9177a641f2b4af020c0e5c73f
Enrique Ajin <enriquesaon@gmail.com>
2024-08-10 22:26:20 +0200

    improvement(remember-focus-requester): Wrap up FocusRequester within remember.

--- app/src/main/java/com/enriqueajin/newsapp/presentation/search_news/SearchNewsScreen.kt
+++ app/src/main/java/com/enriqueajin/newsapp/presentation/search_news/SearchNewsScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -48,7 +49,7 @@ fun SearchNewsScreen(
 ) {
 
     var query by rememberSaveable { mutableStateOf("") }
-    val focusRequest = FocusRequester()
+    val focusRequest = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
 
     LaunchedEffect(Unit) {
