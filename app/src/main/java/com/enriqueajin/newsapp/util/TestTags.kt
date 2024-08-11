d92208dff4b9d6ae007b1d5160d49d46dbe74ba8
Enrique Ajin <enriquesaon@gmail.com>
2024-08-11 17:11:02 +0200

    feat(bottom-bar-test): Create BottomBarTest to test the correct selection of the tabs and check if the icons are selected or unselected. Delete ExampleInstrumentedTest.kt.

---
+++ app/src/main/java/com/enriqueajin/newsapp/util/TestTags.kt
@@ -0,0 +1,9 @@
+package com.enriqueajin.newsapp.util
+
+object TestTags {
+
+    const val BOTTOM_BAR_ITEM = "bottom_bar_item"
+    const val BOTTOM_BAR_ITEM_HOME = "bottom_bar_item0"
+    const val BOTTOM_BAR_ITEM_SEARCH = "bottom_bar_item1"
+    const val BOTTOM_BAR_ITEM_FAVORITES = "bottom_bar_item2"
+}
