368ac0059e3c08f95f340ae50e557350936ff6c0
Enrique Ajin <enriquesaon@gmail.com>
2025-11-16 18:33:29 +0100

    Update navigation, kotlin, compiler & gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,17 @@ plugins {
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.enriqueajin.newsapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.enriqueajin.newsapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
