31b94e453f97ab635f954d88ec4155282666848c
Enrique Ajin <68206669+enriqueajin@users.noreply.github.com>
2025-12-19 20:25:29 +0100

    Make OnInit InternalEvent. Delete pagingItems from State and keep them separately in VM. Leave State with empty default values (#26)

--- app/src/main/java/com/enriqueajin/newsapp/presentation/home/HomeContract.kt
+++ app/src/main/java/com/enriqueajin/newsapp/presentation/home/HomeContract.kt
@@ -1,44 +1,30 @@
 package com.enriqueajin.newsapp.presentation.home
 
-import androidx.paging.PagingData
 import com.enriqueajin.newsapp.domain.model.Article
 import com.enriqueajin.newsapp.presentation.UiText
 import com.enriqueajin.newsapp.util.Constants.CATEGORIES_INITIAL_VALUE
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 interface HomeContract {
 
     sealed class Event {
-        data object OnInit: Event()
         data class OnCategoryChange(val category: String): Event()
         data class OnKeywordChange(val keyword: String): Event()
         data class OnItemClick(val article: Article): Event()
         data class OnSeeAllClick(val keyword: String): Event()
-        data object OnRetryClick: Event()
+        data object OnRetry: Event()
     }
 
     data class State(
-        val loading: Boolean,
-        val error: UiText,
-        val latestArticles: List<Article>,
-        val articlesByKeyword: List<Article>,
-        val newsByCategory: Flow<PagingData<Article>>,
-        val keyword: String,
-        val category: String,
-    ) {
+        val loading: Boolean = true,
+        val error: UiText = UiText.DynamicString(""),
+        val latestArticles: List<Article> = emptyList(),
+        val articlesByKeyword: List<Article> = emptyList(),
+        val keyword: String = "",
+        val category: String = CATEGORIES_INITIAL_VALUE,
+    )
 
-        companion object {
-            fun empty() = State(
-                loading = true,
-                error = UiText.DynamicString(""),
-                latestArticles = emptyList(),
-                articlesByKeyword = emptyList(),
-                newsByCategory = flowOf(PagingData.empty()),
-                keyword = "",
-                category = CATEGORIES_INITIAL_VALUE
-            )
-        }
+    sealed class InternalEvent {
+        data object OnInit: InternalEvent()
     }
 
     sealed class Effect {
@@ -46,4 +32,3 @@ interface HomeContract {
         data class NavigateToArticlesWithKeyword(val keyword: String): Effect()
     }
 }
-
--- app/src/main/java/com/enriqueajin/newsapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/enriqueajin/newsapp/presentation/home/HomeViewModel.kt
@@ -2,7 +2,6 @@ package com.enriqueajin.newsapp.presentation.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.enriqueajin.newsapp.domain.Result
 import com.enriqueajin.newsapp.domain.use_case.GetNewsByCategoryUseCase
@@ -10,18 +9,21 @@ import com.enriqueajin.newsapp.domain.use_case.GetNewsByKeywordUseCase
 import com.enriqueajin.newsapp.presentation.UiText
 import com.enriqueajin.newsapp.presentation.asUiText
 import com.enriqueajin.newsapp.presentation.home.HomeContract.Effect
-import com.enriqueajin.newsapp.presentation.home.HomeContract.Effect.*
 import com.enriqueajin.newsapp.presentation.home.HomeContract.Event
+import com.enriqueajin.newsapp.presentation.home.HomeContract.InternalEvent
 import com.enriqueajin.newsapp.presentation.home.HomeContract.State
 import com.enriqueajin.newsapp.util.KeywordProvider
+import com.enriqueajin.newsapp.util.emitEffect
 import com.enriqueajin.newsapp.util.updateState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -31,37 +33,42 @@ class HomeViewModel @Inject constructor(
     private val getNewsByKeywordUseCase: GetNewsByKeywordUseCase
 ) : ViewModel() {
 
-    private val _uiState: MutableStateFlow<State> = MutableStateFlow(State.empty())
+    private val _uiState: MutableStateFlow<State> = MutableStateFlow(State())
     val uiState = _uiState.asStateFlow()
 
     private val _uiEffects: MutableSharedFlow<Effect> = MutableSharedFlow()
     val uiEffects = _uiEffects.asSharedFlow()
 
-    private var lastEventAttempted: Event? = null
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val lazyArticlesByCategory = _uiState
+        .map { it.category }
+        .distinctUntilChanged()
+        .flatMapLatest { category ->
+            getNewsByCategoryUseCase(category)
+        }
+        .cachedIn(viewModelScope)
 
     init {
-        onEvent(Event.OnInit)
+        pushInternal(InternalEvent.OnInit)
     }
 
     fun onEvent(event: Event) {
         when (event) {
-            Event.OnInit -> getInitialArticles(event)
-            is Event.OnCategoryChange -> getArticlesByCategory(category = event.category)
+            is Event.OnCategoryChange -> _uiState.updateState { it.copy(category = event.category) }
             is Event.OnKeywordChange -> getArticlesByKeyword(keyword = event.keyword)
-            is Event.OnItemClick -> emitEffect(NavigateToArticleDetail(event.article))
-            is Event.OnSeeAllClick -> emitEffect(NavigateToArticlesWithKeyword(event.keyword))
-            Event.OnRetryClick -> lastEventAttempted?.let { onEvent(it) }
+            is Event.OnItemClick -> _uiEffects.emitEffect(viewModelScope) { Effect.NavigateToArticleDetail(event.article) }
+            is Event.OnSeeAllClick -> _uiEffects.emitEffect(viewModelScope) { Effect.NavigateToArticlesWithKeyword(event.keyword) }
+            Event.OnRetry -> getInitialArticles()
         }
     }
 
-    private fun emitEffect(effect: Effect) {
-        viewModelScope.launch {
-            _uiEffects.emit(effect)
+    private fun pushInternal(internalEvent: InternalEvent) {
+        when(internalEvent) {
+            InternalEvent.OnInit -> getInitialArticles()
         }
     }
 
-    private fun getInitialArticles(event: Event) {
-        lastEventAttempted = event
+    private fun getInitialArticles() {
         getLatestArticles()
         getArticlesByKeyword(KeywordProvider.getRandomKeyword())
     }
@@ -83,26 +90,6 @@ class HomeViewModel @Inject constructor(
                         )
                     }
                 }
-            }
-        }
-    }
-
-    private fun getArticlesByCategory(category: String) {
-        _uiState.updateState { it.copy(loading = true) }
-        viewModelScope.launch {
-            val articles = getNewsByCategoryUseCase(category)
-                .cachedIn(viewModelScope)
-                .stateIn(
-                    scope = viewModelScope,
-                    started = SharingStarted.WhileSubscribed(5000L),
-                    initialValue = PagingData.empty()
-                )
-            _uiState.updateState {
-                it.copy(
-                    newsByCategory = articles,
-                    category = category,
-                    loading = false
-                )
             }
         }
     }
