5bca6dcf478f1caea878604cc12d4674d3a8b99b
Enrique Ajin <enriquesaon@gmail.com>
2025-12-19 20:57:44 +0100

    Create reusable Loading composable & use it in HomeScreen

--- app/src/main/java/com/enriqueajin/newsapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/enriqueajin/newsapp/presentation/home/HomeScreen.kt
@@ -1,16 +1,12 @@
 package com.enriqueajin.newsapp.presentation.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +18,7 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.enriqueajin.newsapp.domain.model.Article
 import com.enriqueajin.newsapp.presentation.Error
+import com.enriqueajin.newsapp.presentation.Loading
 import com.enriqueajin.newsapp.presentation.home.HomeContract.Effect
 import com.enriqueajin.newsapp.presentation.home.HomeContract.Event
 import com.enriqueajin.newsapp.presentation.home.HomeContract.State
@@ -116,15 +113,6 @@ private fun HomeScreen(
             }
         }
 
-    }
-}
-
-@Composable
-private fun Loading(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize()) {
-        CircularProgressIndicator(modifier = Modifier
-            .align(Alignment.Center)
-        )
     }
 }
 
